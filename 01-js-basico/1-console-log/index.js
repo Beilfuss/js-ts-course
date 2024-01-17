fad3a5ba40510543b3ede187ff90048cd72fa5f2
Beilfuss <93745995+Beilfuss@users.noreply.github.com>
2024-01-17 10:13:51 -0300

    console.log

---
+++ 01-js-basico/1-console-log/index.js
@@ -0,0 +1,5 @@
+console.log('Hello, World!'); /* Método "log" dentro do objeto "console" */
+console.log("Hello, World!");
+console.log(`Hello, World!`); /* Nesse caso, pode-se usar tanto aspas simples quanto duplas dentro da string */
+
+console.log(35, 15.85); /* "Number" em JS, não há separação entre int e float */
