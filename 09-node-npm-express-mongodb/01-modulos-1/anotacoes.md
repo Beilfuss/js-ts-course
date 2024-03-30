e96d088bc474d1f8136854cb3a718bc8185fb4fe
Beilfuss <93745995+Beilfuss@users.noreply.github.com>
2024-03-29 22:34:37 -0300

    modulos 1

---
+++ 09-node-npm-express-mongodb/01-modulos-1/anotacoes.md
@@ -0,0 +1,2 @@
+Nessa aula veremos o sistema de módulos do Node, que diverge do sistema de módulos do ES6.
+
---
+++ 09-node-npm-express-mongodb/01-modulos-1/app.js
@@ -0,0 +1,6 @@
+const nome = 'Noel';
+const sobrenome = 'Gallagher';
+
+const falaNome = () => {
+    console.log(nome, sobrenome);
+};
