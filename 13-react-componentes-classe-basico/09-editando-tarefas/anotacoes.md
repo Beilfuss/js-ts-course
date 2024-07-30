e2d6311d498501ed7e055033485fc172f6fb1828
Beilfuss <matheusdicimb@gmail.com>
2024-07-30 10:06:42 -0300

    Editando tarefas

---
+++ 13-react-componentes-classe-basico/09-editando-tarefas/anotacoes.md
@@ -0,0 +1,7 @@
+# Editando tarefas
+
+- Vamos fazer a funcionalidade de editar tarefas;
+- Adicionar um método no botão de editar;
+- Recuperar o índice do elemento clicado;
+- Jogar a tarefa no input;
+- Editar a tarefa na lista de tarefas.
---
+++ 13-react-componentes-classe-basico/09-editando-tarefas/src/components/Main.js
@@ -0,0 +1,110 @@
+import React, { Component } from "react";
+
+// Form
+import { FaPlus } from 'react-icons/fa'; // import da "font-awesome" para usar ícones
+
+// Tarefas
+import { FaEdit, FaWindowClose } from 'react-icons/fa'; 
+
+import './Main.css';
+
+export default class Main extends Component {
+    state = { // estado do componente. Todas as chaves aqui dentro vão ser o estado do componente. Tudo o que mudar nesse estado reflete no render
+        novaTarefa: "", // inicializa o estado
+        tarefas: [],
+        index: -1 // se o index for -1, não está editando
+    };
+
+    handleSubmit = (e) => {
+        e.preventDefault(); // previne o comportamento padrão do formulário
+        const { tarefas, index } = this.state; // Destructuring. Equivale a const tarefas = this.state.tarefas;
+        let { novaTarefa } = this.state;
+        novaTarefa = novaTarefa.trim(); // remove espaços em branco no início e no final
+
+        if (tarefas.indexOf(novaTarefa) !== -1) return; // se a tarefa já existe, não adiciona
+
+        const novasTarefas = [...tarefas]; // copia as tarefas atuais
+
+        if (index === -1) { // se não está editando
+            this.setState({
+                tarefas: [...novasTarefas, novaTarefa], // adiciona a nova tarefa
+                novaTarefa: "", // limpa o input
+            });
+        } else {
+            novasTarefas[index] = novaTarefa;
+
+            this.setState({
+                tarefas: [...novasTarefas],
+                index: -1,
+            });
+        }
+
+    };
+
+
+    handleChange = (e) => { // método como arrow function para ter o this do componente (bind do this)
+        this.setState({
+            novaTarefa: e.target.value // e.target é o input. value é o valor do input
+        });
+    }
+
+    handleEdit = (e, index) => {
+        const { tarefas } = this.state;
+
+        this.setState({
+            index, // index: index
+            novaTarefa: tarefas[index], // preenche o input com a tarefa que está sendo editada
+        });
+    } 
+
+    handleDelete = (e, index) => {
+        const { tarefas } = this.state;
+        const novasTarefas = [...tarefas];
+        novasTarefas.splice(index, 1); // remove 1 elemento a partir do index
+
+        this.setState({
+            tarefas: [...novasTarefas],
+        });
+    } 
+
+    render() {
+
+        const { novaTarefa, tarefas } = this.state; // Destructuring. Equivale a const novaTarefa = this.state.novaTarefa;
+
+        return (
+            <div className="main"> {/* "className" porque "class" é uma palavra reservada do JS */} 
+                <h1>Lista de tarefas</h1>
+
+                <form onSubmit={this.handleSubmit} action="#" className="form">
+                    <input 
+                        onChange={this.handleChange} 
+                        type="text" 
+                        value={novaTarefa}
+                    />
+                    <button type="submit">
+                        <FaPlus /> {/* Ícone de adicionar */}
+                    </button>
+                </form>
+
+                <ul className="tarefas">
+                    {tarefas.map((tarefa, index) => (
+                        <li key={tarefa}> {/* key é um identificador único, o React obriga isso */}
+                            {tarefa}
+                            <span>
+                                <FaEdit 
+                                    className="edit"
+                                    onClick={(e) => this.handleEdit(e, index)} 
+                                />
+                                <FaWindowClose 
+                                    onClick={(e) => this.handleDelete(e, index)} 
+                                    className="delete" 
+                                />
+                            </span>
+                        </li> 
+                    ))}
+                </ul>
+
+            </div> 
+        );
+    }
+}
