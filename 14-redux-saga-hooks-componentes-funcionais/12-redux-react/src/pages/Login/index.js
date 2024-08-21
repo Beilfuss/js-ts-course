3ae9c73072da96c8dcf39b2edbb98d44eea1956c
Beilfuss <matheusdicimb@gmail.com>
2024-08-21 14:41:26 -0300

    Redux + React

---
+++ 14-redux-saga-hooks-componentes-funcionais/12-redux-react/src/pages/Login/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+import { Container } from '../../styles/GlobalStyles';
+import { Title, Paragrafo } from './styled';
+
+export default function Login() {
+  return (
+    <Container>
+      <Title>
+        Login
+        <small>Olá!</small>
+      </Title>
+      <Paragrafo>Lorem ipsum dolor sit amet.</Paragrafo>
+      <button type='button'>Enviar</button>
+    </Container>
+  );
+}
