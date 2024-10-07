79060fb3bbeeeb057dcab61afee790effd3582b5
Beilfuss <matheusdicimb@gmail.com>
2024-10-06 22:57:53 -0300

    Componente Loading

--- 14-redux-saga-hooks-componentes-funcionais/22-componente-loading/src/components/Loading/index.js
+++ 14-redux-saga-hooks-componentes-funcionais/22-componente-loading/src/components/Loading/index.js
@@ -1,19 +1,6 @@
-import React from 'react';
-import { FaHome, FaSignInAlt, FaUserAlt } from 'react-icons/fa';
-import { Link } from 'react-router-dom';
+import React from 'react'
+import PropTypes from 'prop-types'
 
-import { Nav } from './styled';
-
-export default function Header() {
-  return (<Nav>
-    <Link to="/">
-      <FaHome size={24} />
-    </Link>
-    <Link to="/register">
-      <FaUserAlt size={24} />
-    </Link>
-    <Link to="/login">
-      <FaSignInAlt size={24} />
-    </Link>
-  </Nav>);
+export default function Loading({ isLoading }) {
+  if (!isLoading) return <></>;
 }
