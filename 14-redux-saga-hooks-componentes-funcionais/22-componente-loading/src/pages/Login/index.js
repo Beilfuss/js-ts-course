8c0da3dc7e3cdf071d5c4826c999933a32ba9638
Beilfuss <matheusdicimb@gmail.com>
2024-10-13 13:39:23 +0300

    Componente Loading

--- 14-redux-saga-hooks-componentes-funcionais/22-componente-loading/src/pages/Login/index.js
+++ 14-redux-saga-hooks-componentes-funcionais/22-componente-loading/src/pages/Login/index.js
@@ -1,17 +1,21 @@
 import React from 'react';
 import { toast } from 'react-toastify';
 import { isEmail } from 'validator';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { get } from 'lodash';
 
 import { Container } from '../../styles/GlobalStyles';
 import { Form } from './styled'
 import * as actions from '../../store/modules/auth/actions'
 
+import Loading from '../../components/Loading';
+
 export default function Login(props) {
   const dispatch = useDispatch();
 
   const prevPath = get(props, 'location.state.prevPath', '/'); // Se não existir prevPath, vai para a raiz
+
+  const isLoading = useSelector(state => state.auth.isLoading);
 
   const [email, setEmail] = React.useState('');
   const [password, setPassword] = React.useState('');
@@ -38,6 +42,8 @@ export default function Login(props) {
 
   return (
     <Container>
+      <Loading isLoading={isLoading} />
+
       <h1>Login</h1>
 
       <Form onSubmit={handleSubmit}>
